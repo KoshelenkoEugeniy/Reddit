8eed7ce420620e314a631f2dfa0cf32af7e3ed7c
Eugene <koshelenkoeugeniy@gmail.com>
2019-07-07 18:13:01 +0300

    Droid. Add login screen

---
+++ Reddit.Droid/Views/Auth/AuthActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Views;
+
+namespace Reddit.Droid.Views.Auth
+{
+    [Activity(Label = "AuthActivity")]
+    public class AuthActivity : MvxActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_auth);
+        }
+    }
+}
